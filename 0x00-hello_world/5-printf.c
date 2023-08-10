ba8bf3e064b31d45bd74390157a85d8d86ddeba2
Irakoze chance <irakoze.antonny@gmail.com>
2023-08-10 09:51:36 -0700

    f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	 printf("with proper grammar, but the outcome is a piece of art,, followed by a new line \n");
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
 	 return (0);
 }

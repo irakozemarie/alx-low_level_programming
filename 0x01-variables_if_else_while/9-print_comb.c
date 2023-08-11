ecabf43694a75f2f7a2bb18b711aec82c626c0dd
Irakoze chance <irakoze.antonny@gmail.com>
2023-08-11 11:54:04 -0700

    fd

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,16 +2,14 @@
  * File: 9-print_comb.c
  * Auth: Brennan D Baraban
  */
-
 #include <stdio.h>
-
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
  */
 int main(void)
-{
+  {
 	int num;
 
 	for (num = 0; num <= 9; num++)
@@ -25,6 +23,5 @@ int main(void)
 	}
 
 	putchar('\n');
-
 	return (0);
-}
+ }

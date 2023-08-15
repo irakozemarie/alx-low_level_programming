9147243735b0e4aeb652a3d29b074f5a82455044
Irakoze chance <irakoze.antonny@gmail.com>
2023-08-15 09:56:44 -0700

    ./2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,19 @@
 #include "main.h"
 /**
- * print_alphabet_x10  - Make alphabet_x10 times in lowercase
- *
- */
+*print_alphabet_x10 - prints alphabet letters
+* ten times
+*desc: The function uses loop to operate
+*Return: no return
+*/
 void print_alphabet_x10(void)
 {
-
-char al;
-int i;
-i = 0;
-while (i < 10)
-{	
-for (al = 'a'; al <= 'z'; al++)
+int counter = 0;
+int letter;
+while (counter++ <= 9)
 {
-_putchar(al);
-}
-_putchar('\n');
-i++;
-}
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 
+_putchar(10);
+}
 }

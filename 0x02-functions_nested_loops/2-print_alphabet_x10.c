8f8259978721cab0f4782dcd7a13696b9bebba17
Irakoze chance <irakoze.antonny@gmail.com>
2023-08-15 00:48:00 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10  - Make alphabet_x10 times in lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+char al;
+int i;
+i=0;
+while (i<10)
+{	
+for (al = 'a'; al <= 'z'; al++)
+{
+_putchar(al);
+}
+_putchar('\n');
+i++;
+}
+}

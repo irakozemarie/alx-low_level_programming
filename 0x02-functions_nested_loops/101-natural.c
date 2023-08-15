d36a899e2a394f5ed4030a60ac8a520ab5c8e0fb
Irakoze chance <irakoze.antonny@gmail.com>
2023-08-15 08:09:17 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -prints the sum of all multiples of 3 or 5 upto 1024
+ * Returns : Always (success)
+ */
+
+int main() {
+    int limit = 1024;
+    int sum = 0;
+
+    for (int i = 1; i < limit; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+
+    printf("The sum of all multiples of 3 or 5 below 1024 is: %d\n", sum);
+
+    return 0;
+}
